985c55886fa32fcd04db61c89289f011fc533778
Nilesh Mandal <nilemandal@yahoo.com>
2025-06-15 17:51:00 +0530

    db setup

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,17 +1,16 @@
+import { prisma } from "@/lib/db";
+import Link from "next/link";
 
 export default async function Page() {
-    const response = await fetch("https://dummyjson.com/posts");
-    const data = await response.json();
+    const posts = await prisma.post.findMany();    
 
     return (
         <div className="text-center pt-12">
             <h1 className="text-3xl capitalize font-bold mb-4"> Posts Page </h1>
             <ul>
-                {data.posts.map((post: { id: number; title: string }) => (
+                {posts.map((post: { id: number; title: string }) => (
                     <li key={post.id} className="mb-2">
-                        <a href={`/posts/${post.id}`} className="text-blue-500 hover:underline">
-                            {post.title}
-                        </a>
+                        <Link href={`/posts/${post.id}`}> {post.title} </Link>
                     </li>
                 ))}
             </ul>
